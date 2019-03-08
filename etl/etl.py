06c4040178a7a25ac68d545f32741eebef156493
SirObi <obi.orciuch@onfido.com>
2019-03-08 09:12:20 +0000

    etl cosmetic (dict comprehension)

--- etl/etl.py
+++ etl/etl.py
@@ -1,4 +1,4 @@
 def transform(legacy_data):
     '''Moves data to new format with letters as keys, and scores as values'''
-    return {letter.lower(): score for (score, letters) in legacy_data.items()
-            for letter in legacy_data[score]}
+    return {letter.lower(): score for score, letters in legacy_data.items()
+            for letter in letters}
