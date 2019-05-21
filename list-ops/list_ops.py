46acbad9dfa2037f4a7ff797603b57ff27ab6b60
Obi Orciuch <obi.orciuch@gmail.com>
2019-05-21 15:58:00 +0100

    list-ops solution (lists, folding, list comprehensions, generators)

---
+++ list-ops/list_ops.py
@@ -0,0 +1,48 @@
+def append(xs, ys):
+    return xs + ys
+
+
+def concat(lists):
+    concatenated = []
+    for item in lists:
+        if type(item) == list:
+            for i in item:
+                concatenated.append(i)
+        else:
+            concatenated.append(item)
+    return concatenated
+
+
+def filter_clone(function, xs):
+    return [s for s in xs if function(s)]
+
+
+def length(xs):
+    return len(xs)
+
+
+def map_clone(function, xs):
+    return [function(s) for s in xs]
+
+
+def foldl(function, xs, acc):
+    if len(xs) == 0:
+        return acc
+    for s in xs:
+        acc = function(acc, s)
+    return acc
+
+
+def foldr(function, xs, acc):
+    if len(xs) == 0:
+        return acc
+    for s in reversed(xs):
+        acc = function(s, acc)
+    return acc
+
+
+def reverse(xs):
+    if len(xs) == 0:
+        return xs
+    else:
+        return [*reversed(xs)]
