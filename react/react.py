17798f016b448ea40fed3bf3094a2eb7742f642c
Obi Orciuch <obi.orciuch@gmail.com>
2019-06-28 16:11:39 +0100

    Save changes react

--- react/react.py
+++ react/react.py
@@ -1,12 +1,34 @@
 class InputCell(object):
     def __init__(self, initial_value):
-        self.value = initial_value
+        self._value = initial_value
+        self.observers = []
 
     def __add__(self, other):
         return self.value + other
 
     def __mul__(self, other):
         return self.value * other
+
+    def register_observer(self, cell):
+        self.observers.append(cell)
+
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if self._value != new_value:
+            self._value = new_value
+            print("calling callbacks")
+            self.notify()
+
+    def notify(self):
+        for o in self.observers:
+            print(type(o))
+            "calling a callback"
+            o.value
 
     # def register_observer(self, cell):
     #     self.observers.append(cell)
@@ -18,6 +40,8 @@ class InputCell(object):
 class ComputeCell(object):
     def __init__(self, inputs, compute_function):
         self.inputs = inputs
+        for input_cell in self.inputs:
+            input_cell.register_observer(self)
         self.compute_function = compute_function
         self._value = compute_function(self.inputs)
         self.callbacks = []
@@ -31,6 +55,7 @@ class ComputeCell(object):
     @property
     def value(self):
         self._value = self.compute_function(self.inputs)
+        print("Compute cell value is", self._value)
         return self._value
 
     @value.setter
@@ -42,8 +67,9 @@ class ComputeCell(object):
             self.notify()
 
     def notify(self):
+        print("notify called")
         for callback in self.callbacks:
-            "calling a callback"
+            print("calling a callback")
             callback(self._value)
 
     def add_callback(self, callback):
